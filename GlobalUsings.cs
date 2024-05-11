17cfc24bbed56a4dc2f03ddb260d967c5972b68f
sunamo.cz_asus <radek.jancik@sunamo.cz>
2024-05-11 11:16:11 +0200

    Some projects are flat and buildable

--- GlobalUsings.cs
+++ GlobalUsings.cs
@@ -2,3 +2,8 @@ global using System.Collections.Generic;
 global using System.Linq;
 global using SunamoValues;
 global using System.Text.RegularExpressions;
+global using System.Text;
+global using System.Collections.Generic;
+global using System.Collections;
+global using System.IO;
+
--- WildcardHelper.cs
+++ WildcardHelper.cs
@@ -3,6 +3,6 @@ public class WildcardHelper
 {
     public static bool IsWildcard(string text)
     {
-        return text.ToCharArray().Any(d => d == AllCharsSE.q) || text.ToCharArray().Any(d => d == AllCharsSE.asterisk);
+        return text.ToCharArray().Any(d => d == AllChars.q) || text.ToCharArray().Any(d => d == AllChars.asterisk);
     }
 }
